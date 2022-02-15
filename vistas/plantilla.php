1a2b59e24e1402d4e64bb5c2f23f4a294c996fd3
fred <fred.saez@hotmail.com>
2022-02-15 16:53:55 -0500

    video10

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -5,8 +5,10 @@
 <head>
  <title>paggina</title>
 
- <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+
+ <!---los logos-->
+ <link rel="apple-touch-icon" href="vistas/img/plantilla/icono.png">
+    <link rel="shortcut icon" href="vistas/img/plantilla/icono.png">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -14,9 +16,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+    
     <link rel="stylesheet" href="vistas/assets/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/
+    html5shiv/3.7.3/html5shiv.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
 
@@ -27,8 +31,9 @@
     <?php
    
    include "modulos/menu.php";
-
+        
     echo '<div id="right-panel" class="right-panel">';
+    /**iniciamos <div id="right-panel" class="right-panel (cabezera) */
     include "modulos/cabezera.php";
    
 
@@ -40,6 +45,7 @@
 
         }
     }
+    
   include "modulos/footer.php";
 
     echo "</div>";
@@ -69,7 +75,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
-    <script src="vistas/js/init/fullcalendar-init.js"></script>
+    <script src="vistas/assets/js/init/fullcalendar-init.js"></script>
  </body>
 </html>
-
