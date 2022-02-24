11776d2502d060624318ec1cd0ee48b94ee2726a
fred <fred.saez@hotmail.com>
2022-02-24 18:04:32 -0500

    error

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -90,5 +90,7 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
     <script src="vistas/assets/js/init/fullcalendar-init.js"></script>
+
+    <script src="vistas/assets/js/init/fullcalendar-init.js"></script>
  </body>
 </html>
